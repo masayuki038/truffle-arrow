fd6b649bc1f12ddee888eed026644db75815c4b2
Masayuki Takahashi <masayuki038@gmail.com>
2020-01-22 22:39:45 +0900

    Close ResultSet and PreparedStatement

--- src/test/java/net/wrap_trap/truffle_arrow/JdbcTest.java
+++ src/test/java/net/wrap_trap/truffle_arrow/JdbcTest.java
@@ -25,29 +25,30 @@ public class JdbcTest {
   @Test
   public void simpleProjection() throws SQLException {
     try (Connection conn = DriverManager.getConnection("jdbc:truffle://localhost:80")) {
-      PreparedStatement pstmt = conn.prepareStatement("select N_NATIONKEY, N_NAME, N_REGIONKEY from NATIONSSF");
-      ResultSet rs = pstmt.executeQuery();
+      try (PreparedStatement pstmt = conn.prepareStatement("select N_NATIONKEY, N_NAME, N_REGIONKEY from NATIONSSF")) {
+        try (ResultSet rs = pstmt.executeQuery()) {
+          ResultSetMetaData meta = rs.getMetaData();
+          int count = meta.getColumnCount();
+          assertThat(count, is(3));
+          assertThat(meta.getColumnName(1), is("N_NATIONKEY"));
+          assertThat(meta.getColumnName(2), is("N_NAME"));
+          assertThat(meta.getColumnName(3), is("N_REGIONKEY"));
 
-      ResultSetMetaData meta = rs.getMetaData();
-      int count = meta.getColumnCount();
-      assertThat(count, is(3));
-      assertThat(meta.getColumnName(1), is("N_NATIONKEY"));
-      assertThat(meta.getColumnName(2), is("N_NAME"));
-      assertThat(meta.getColumnName(3), is("N_REGIONKEY"));
+          List<String> results = Lists.newArrayList();
+          while (rs.next()) {
+            results.add(IntStream.rangeClosed(1, count)
+                          .mapToObj(i -> {
+                            try { return rs.getObject(i); }
+                            catch (Exception e) { throw new RuntimeException(e); }
+                          }).map(o -> o == null ? "" : o.toString())
+                          .collect(Collectors.joining("\t")));
+          }
 
-      List<String> results = Lists.newArrayList();
-      while (rs.next()) {
-        results.add(IntStream.rangeClosed(1, count)
-                      .mapToObj(i -> {
-                        try { return rs.getObject(i); }
-                        catch (Exception e) { throw new RuntimeException(e); }
-                      }).map(o -> o == null ? "" : o.toString())
-                      .collect(Collectors.joining("\t")));
+          assertThat(results.size(), is(25));
+          assertThat(results.get(0), is("0\tALGERIA\t0"));
+          assertThat(results.get(24), is("24\tUNITED STATES\t1"));
+        }
       }
-
-      assertThat(results.size(), is(25));
-      assertThat(results.get(0), is("0\tALGERIA\t0"));
-      assertThat(results.get(24), is("24\tUNITED STATES\t1"));
     }
   }
 }
