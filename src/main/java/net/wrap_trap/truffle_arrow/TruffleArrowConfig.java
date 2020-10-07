984df34b2abfc86578ba0025a6c51f65e30c2510
Masayuki Takahashi <masayuki038@gmail.com>
2020-10-08 00:50:29 +0900

    Add lightbend/config to load an application.conf for setting directory root path

--- src/main/java/net/wrap_trap/truffle_arrow/TruffleArrowConfig.java
+++ src/main/java/net/wrap_trap/truffle_arrow/TruffleArrowConfig.java
@@ -1,6 +1,8 @@
 package net.wrap_trap.truffle_arrow;
 
 import com.google.common.collect.ImmutableList;
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import net.wrap_trap.truffle_arrow.storage.columnar.ArrowColumnarSchema;
 import org.apache.calcite.adapter.java.JavaTypeFactory;
 import org.apache.calcite.jdbc.CalciteSchema;
@@ -11,10 +13,16 @@ import org.apache.calcite.sql.fun.SqlStdOperatorTable;
 import org.apache.calcite.sql.validate.SqlConformance;
 import org.apache.calcite.sql.validate.SqlValidator;
 import org.apache.calcite.sql.validate.SqlValidatorImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 
 public class TruffleArrowConfig {
+
+  private static final Logger log = LoggerFactory.getLogger(TruffleArrowConfig.class);
+
+  private static final String CONFIG_ROOT_PATH = "data.root";
 
   public static final TruffleArrowConfig INSTANCE = new TruffleArrowConfig();
 
@@ -39,9 +47,14 @@ public class TruffleArrowConfig {
   }
 
   void reload() {
+    Config config = ConfigFactory.load();
     SqlOperatorTable operatorTable = SqlStdOperatorTable.instance();
-    // TODO
-    ArrowSchema schema = new ArrowColumnarSchema(new File("target/classes/samples/files"));
+    String rootPath = config.getString(CONFIG_ROOT_PATH);
+    if (rootPath == null) {
+      log.warn(String.format("'%s' is not found. Set it to '.'", CONFIG_ROOT_PATH));
+      rootPath = ".";
+    }
+    ArrowSchema schema = new ArrowColumnarSchema(new File(rootPath));
     CalciteSchema rootSchema = CalciteSchema.createRootSchema(false, true, "SAMPLES", schema);
     this.typeFactory = new JavaTypeFactoryImpl();
     this.catalogReader =
