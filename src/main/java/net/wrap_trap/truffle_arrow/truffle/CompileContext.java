85fc751c479c28dfdbceac8ffec9ac7e79ccfb71
Masayuki Takahashi <masayuki038@gmail.com>
2020-10-17 22:46:58 +0900

    Remove unnecessary imports

--- src/main/java/net/wrap_trap/truffle_arrow/truffle/CompileContext.java
+++ src/main/java/net/wrap_trap/truffle_arrow/truffle/CompileContext.java
@@ -1,10 +1,6 @@
 package net.wrap_trap.truffle_arrow.truffle;
 
-import org.apache.arrow.vector.FieldVector;
-import org.apache.arrow.vector.UInt4Vector;
-
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 public class CompileContext {
