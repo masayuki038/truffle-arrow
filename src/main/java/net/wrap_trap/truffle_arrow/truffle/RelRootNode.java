52edd5a74dd50046438499f91b24350d87f89015
Masayuki Takahashi <masayuki038@gmail.com>
2021-03-14 14:26:25 +0900

    Add an error logging on RelRootNode

--- src/main/java/net/wrap_trap/truffle_arrow/truffle/RelRootNode.java
+++ src/main/java/net/wrap_trap/truffle_arrow/truffle/RelRootNode.java
@@ -1,14 +1,18 @@
 package net.wrap_trap.truffle_arrow.truffle;
 
+import net.wrap_trap.truffle_arrow.ArrowUtils;
 import net.wrap_trap.truffle_arrow.TruffleArrowLanguage;
 
 import com.oracle.truffle.api.frame.FrameDescriptor;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.RootNode;
-import com.oracle.truffle.api.nodes.UnexpectedResultException;
 import org.apache.arrow.vector.VectorSchemaRoot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RelRootNode extends RootNode {
+
+  private static final Logger log = LoggerFactory.getLogger(RelRootNode.class);
 
   @Child
   private RowSource delegate;
@@ -24,7 +28,8 @@ public class RelRootNode extends RootNode {
     try {
       VectorSchemaRoot[] results = this.delegate.execute();
       return new Result(results);
-    } catch (UnexpectedResultException e) {
+    } catch (Exception e) {
+      log.error("Failed to RelRRootNode#execute", e);
       throw new RuntimeException(e);
     }
   }
