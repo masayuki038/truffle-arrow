706a46732b6c18c915d65fe087e7e7c718b1b094
Masayuki Takahashi <masayuki038@gmail.com>
2021-04-06 22:09:12 +0900

    Add a configuration for logging around allocation size

--- src/main/java/net/wrap_trap/truffle_arrow/ArrowUtils.java
+++ src/main/java/net/wrap_trap/truffle_arrow/ArrowUtils.java
@@ -32,16 +32,23 @@ public class ArrowUtils {
 
   private static final Logger log = LoggerFactory.getLogger(ArrowUtils.class);
   private static final String CONFIG_ALLOCATOR_SIZE = "allocator.initial.size";
+  private static final String CONFIG_ALLOCATOR_DEBUG_LOG = "allocator.debug-log";
 
   private static RootAllocator rootAllocator = new RootAllocator(Long.MAX_VALUE);
 
   public static BufferAllocator createAllocator(String desc) {
     Config config = ConfigFactory.load();
     int size = config.getInt(CONFIG_ALLOCATOR_SIZE);
-    log.debug(String.format("createAllocator [%s] (before) %s", desc, rootAllocator.toVerboseString()));
+    boolean enableDebugLog = config.getBoolean(CONFIG_ALLOCATOR_DEBUG_LOG);
+
+    if (enableDebugLog) {
+      log.debug(String.format("createAllocator [%s] (before) %s", desc, rootAllocator.toVerboseString()));
+    }
     BufferAllocator newBuffer = rootAllocator.newChildAllocator(
       Thread.currentThread().getName(), size, Integer.MAX_VALUE);
-    log.debug(String.format("createAllocator [%s] (after) %s", desc, rootAllocator.toVerboseString()));
+    if (enableDebugLog) {
+      log.debug(String.format("createAllocator [%s] (after) %s", desc, rootAllocator.toVerboseString()));
+    }
     return newBuffer;
   }
 
